3d707e7aa342ab99adce81cd4abe925a75c27186
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-12-03 19:38:07 +0500

    Add project files.

---
+++ Tyuiu.AshirbakievAR.Sprint6.Task0.V4.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AshirbakievAR.Sprint6.Task0.V4.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.AshirbakievAR.Sprint6.Task0.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AshirbakievAR.Sprint6.Task0.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AshirbakievAR.Sprint6.Task0.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("874d1bee-09a4-4bf6-9150-bb91a4289e97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
